5df45e62ed5cc2547a1da39587c96a376f3c41e1
lilxu1819 <lilxu1819@gmail.com>
2026-07-31 01:25:22 +0800

    chore: add sw.js

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'lilxu-workbench-v33';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css?v=33',
+  './app.js?v=33',
+  './manifest.json?v=33',
+  './icons/icon-v15.svg?v=33',
+  './icons/icon-192-v15.png?v=33',
+  './icons/icon-512-v15.png?v=33'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  // 全部走网络优先，彻底解决缓存导致看不到更新的问题
+  event.respondWith(
+    fetch(event.request).then((res) => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+      return res;
+    }).catch(() =>
+      caches.match(event.request).then((cached) => cached || caches.match('./index.html'))
+    )
+  );
+});
+
+// 点击通知时聚焦到已打开的页面
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if ('focus' in client) return client.focus();
+      }
+      if (clients.openWindow) return clients.openWindow('/');
+    })
+  );
+});
